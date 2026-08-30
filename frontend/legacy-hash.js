be805b86bcc0411d8c1e8922f1201cc597184ac3
Doron <doron@callvu.com>
2026-08-30 08:28:00 +0300

    Add legacy hash helper (Attest gate test - weak MD5 on purpose)

---
+++ frontend/legacy-hash.js
@@ -0,0 +1,8 @@
+﻿// Temporary file to exercise the Attest PR gate - do not merge.
+const crypto = require('crypto');
+
+function hashEntryId(s) {
+  return crypto.createHash('md5').update(s).digest('hex');
+}
+
+module.exports = { hashEntryId };

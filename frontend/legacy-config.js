c35ae862f5b5c5f6ac204de6715837adb93d3828
Doron <doron@callvu.com>
2026-08-30 08:47:49 +0300

    Add legacy config (Attest gate test - fake hardcoded secret)

---
+++ frontend/legacy-config.js
@@ -0,0 +1,5 @@
+﻿// Temporary file to exercise the Attest PR gate - do not merge.
+// This is a FAKE credential planted to trigger secret detection.
+module.exports = {
+  diaryApiKey: "dk_live_9zX2mQ8vLp4Wc7Bn3TyU6sHd1RaE5fKj",
+};
